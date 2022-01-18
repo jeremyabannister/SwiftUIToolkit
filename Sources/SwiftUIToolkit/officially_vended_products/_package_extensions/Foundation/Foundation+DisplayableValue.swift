7ab692336e0da6d23cc9831160c7d875ac517828
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-01-18 13:50:42 +0100

    Added conformance of various `Foundation` types to `DisplayableValue`.

---
+++ Sources/SwiftUIToolkit/officially_vended_products/_package_extensions/Foundation/Foundation+DisplayableValue.swift
@@ -0,0 +1,16 @@
+//
+//  Foundation+DisplayableValue.swift
+//  
+//
+//  Created by Jeremy Bannister on 1/18/22.
+//
+
+///
+extension Array: DisplayableValue { }
+extension Dictionary: DisplayableValue { }
+extension Dictionary.Keys: DisplayableValue { }
+extension Optional: DisplayableValue { }
+extension Result: DisplayableValue { }
+extension Set: DisplayableValue { }
+extension String: DisplayableValue { }
+extension UUID: DisplayableValue { }
