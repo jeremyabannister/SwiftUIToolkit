2644dc92482c78f0ba25e5f8e47f0865f2aea835
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-01-19 19:49:29 +0100

    Added `View.optionalOverlay(value:transition:alignment:overlay:)` and `View.conditionalOverlay(condition:transition:alignment:overlay:)`.

---
+++ Sources/SwiftUIToolkit/officially_vended_products/conditional_overlay/View+conditionalOverlay.swift
@@ -0,0 +1,30 @@
+//
+//  View+conditionalOverlay.swift
+//  
+//
+//  Created by Jeremy Bannister on 1/19/22.
+//
+
+///
+public extension View {
+    
+    ///
+    func conditionalOverlay
+        <Overlay: View>
+            (condition: Bool,
+             transition: AnyTransition = .scale(scale: 0.00001),
+             alignment: Alignment = .center,
+             @ViewBuilder overlay: ()->Overlay)
+                -> some View {
+        
+        ///
+        self.optionalOverlay(
+            value: condition ? Void?.some(()) : Void?.none,
+            transition: transition,
+            alignment: alignment,
+            overlay: { _ in
+                overlay()
+            }
+        )
+    }
+}
---
+++ Sources/SwiftUIToolkit/officially_vended_products/optional_overlay/View+optionalOverlay.swift
@@ -0,0 +1,29 @@
+//
+//  View+optionalOverlay.swift
+//  
+//
+//  Created by Jeremy Bannister on 1/19/22.
+//
+
+///
+public extension View {
+    
+    ///
+    func optionalOverlay
+        <Overlay: View,
+         DisplayedValue>
+            (value: Optional<DisplayedValue>,
+             transition: AnyTransition = .scale(scale: 0.00001),
+             alignment: Alignment = .center,
+             @ViewBuilder overlay: (DisplayedValue)->Overlay)
+                -> some View {
+        
+        ///
+        self.overlay(
+            value
+                .map(overlay)?
+                .transition(transition),
+            alignment: alignment
+        )
+    }
+}
