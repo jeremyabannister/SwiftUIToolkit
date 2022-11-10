5088786bc24fed0a89252621870c65ced79b1ee7
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-11-10 14:14:20 +0100

    Added two overloads `View.expand(along:aligment:)` along with two corresponding overload disambiguators, `HorizontalAxisOverloadDisambiguator` and `VerticalAxisOverloadDisambiguator`.

--- Sources/SwiftUIToolkit/officially_vended_products/expand/View+expand.swift
+++ Sources/SwiftUIToolkit/officially_vended_products/expand/View+expand.swift
@@ -9,6 +9,46 @@
 public extension View {
     
     ///
+    func expand
+        (along axis: HorizontalAxisOverloadDisambiguator,
+         alignment: HorizontalAlignment = .center)
+    -> some View {
+        HStack {
+            Spacer(minLength: 0)
+                .nullify(if: alignment.isEqual(to: .leading))
+            self
+            Spacer(minLength: 0)
+                .nullify(if: alignment.isEqual(to: .trailing))
+        }
+    }
+    
+    ///
+    func expand
+        (along axis: VerticalAxisOverloadDisambiguator,
+         alignment: VerticalAlignment = .center)
+    -> some View {
+        VStack {
+            Spacer(minLength: 0)
+                .nullify(if: alignment.isEqual(to: .top))
+            self
+            Spacer(minLength: 0)
+                .nullify(if: alignment.isEqual(to: .bottom))
+        }
+    }
+}
+
+///
+public struct HorizontalAxisOverloadDisambiguator {
+    public static var horizontal: Self { .init() }
+}
+public struct VerticalAxisOverloadDisambiguator {
+    public static var vertical: Self { .init() }
+}
+
+///
+public extension View {
+    
+    ///
     func expand (alignment: Alignment = .center) -> some View {
         ZStack(alignment: alignment) {
             Color.clear
