01f5f0785cdb35351c864dae21a1d73fba5e6345
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-09 02:10:44 +0200

    Bumped version range requirement for `FoundationToolkit` to `0.6.0 ..< 0.7.0`.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         ///
         .package(
             url: "https://github.com/jeremyabannister/FoundationToolkit",
-            "0.5.0" ..< "0.6.0"
+            "0.6.0" ..< "0.7.0"
         ),
         
         ///
