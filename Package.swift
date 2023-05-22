26912fa712b9b383139079952ef7eb9c637656de
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-05-22 20:51:43 +0200

    Updated to `FoundationToolkit` version `0.7.0`.

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         ///
         .package(
             url: "https://github.com/jeremyabannister/FoundationToolkit",
-            "0.6.0" ..< "0.7.0"
+            "0.7.0" ..< "0.8.0"
         ),
         
         ///
