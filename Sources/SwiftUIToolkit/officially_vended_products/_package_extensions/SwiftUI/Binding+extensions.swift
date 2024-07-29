e50db5ecf1aceda9aef99b957eaa0314d52502d8
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-07-29 14:05:28 +0200

    Removed the deprecated method `Binding.unwrap()`.

--- Sources/SwiftUIToolkit/officially_vended_products/_package_extensions/SwiftUI/Binding+extensions.swift
+++ Sources/SwiftUIToolkit/officially_vended_products/_package_extensions/SwiftUI/Binding+extensions.swift
@@ -35,27 +35,6 @@ extension Binding {
 extension Binding {
     
     ///
-    @available(*, deprecated, message: "This method has been renamed to .unwrapped().")
-    public func unwrap<Wrapped>() -> Binding<Wrapped>?
-        where Value == Optional<Wrapped> {
-        
-        ///
-        if let value = self.wrappedValue {
-            return
-                Binding<Wrapped>(
-                    get: { self.wrappedValue ?? value },
-                    set: { newValue in
-                        if self.wrappedValue.isNotNil {
-                            self.wrappedValue = .some(newValue)
-                        }
-                    }
-                )
-        } else {
-            return nil
-        }
-    }
-    
-    ///
     public func forceUnwrap<Wrapped>() -> Binding<Wrapped>
         where Value == Optional<Wrapped> {
         
