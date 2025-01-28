14e73a790f5391b1013773db04a5bc88e40b498a
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2025-01-28 18:52:58 +0100

    feat: allow easy access to common points of a CGRect

--- Sources/SwiftUIToolkit/officially_vended_products/_package_extensions/CoreGraphics/CGRect+extensions.swift
+++ Sources/SwiftUIToolkit/officially_vended_products/_package_extensions/CoreGraphics/CGRect+extensions.swift
@@ -5,5 +5,38 @@
 //  Created by Jeremy Bannister on 1/18/22.
 //
 
-///
 extension CGRect: @retroactive ExpressionErgonomic { }
+
+extension CGRect {
+    public var leadingPoint: CGPoint {
+        .init(x: minX, y: midY)
+    }
+    
+    public var topLeadingPoint: CGPoint {
+        .init(x: minX, y: minY)
+    }
+    
+    public var topPoint: CGPoint {
+        .init(x: midX, y: minY)
+    }
+    
+    public var topTrailingPoint: CGPoint {
+        .init(x: maxX, y: minY)
+    }
+    
+    public var trailingPoint: CGPoint {
+        .init(x: maxX, y: midY)
+    }
+    
+    public var bottomTrailingPoint: CGPoint {
+        .init(x: maxX, y: maxY)
+    }
+    
+    public var bottomPoint: CGPoint {
+        .init(x: midX, y: maxY)
+    }
+    
+    public var bottomLeadingPoint: CGPoint {
+        .init(x: minX, y: maxY)
+    }
+}
