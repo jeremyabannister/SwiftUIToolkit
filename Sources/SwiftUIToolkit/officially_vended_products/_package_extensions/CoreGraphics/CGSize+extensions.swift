ddb657d54d661e2c704c1839cbab3572659de609
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2025-03-16 00:33:12 +0100

    feat: add CGSize.init(_:_:)

--- Sources/SwiftUIToolkit/officially_vended_products/_package_extensions/CoreGraphics/CGSize+extensions.swift
+++ Sources/SwiftUIToolkit/officially_vended_products/_package_extensions/CoreGraphics/CGSize+extensions.swift
@@ -1,26 +1,16 @@
-//
-//  CGSize+extensions.swift
-//  
-//
-//  Created by Jeremy Bannister on 1/18/22.
-//
-
-///
 extension CGSize: @retroactive ExpressionErgonomic { }
 
-///
 extension CGSize {
-    
-    ///
     public static func square(_ sideLength: CGFloat) -> Self {
         .init(width: sideLength, height: sideLength)
     }
+    
+    public init(_ width: CGFloat, _ height: CGFloat) {
+        self.init(width: width, height: height)
+    }
 }
 
-///
 extension CGSize {
-    
-    ///
     public var aspectRatio: CGFloat {
         width/height
     }
